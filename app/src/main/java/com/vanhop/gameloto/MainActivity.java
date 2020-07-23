ff0b83024d4e1cc9d75082628a08205506b8d5a7
vanhop1993 <dauvanhop@gmail.com>
2020-07-23 15:16:22 +0700

    Initial commit

---
+++ app/src/main/java/com/vanhop/gameloto/MainActivity.java
@@ -0,0 +1,37 @@
+package com.vanhop.gameloto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*int arrayList[];
+        arrayList = new int[100];
+        double random;
+        int max, int min
+        for (int i = 0; i <= 100; i++) {
+            for (int j = 0; ; j++) {
+                int flag = 0;
+                random = Math.floor(Math.random() * (max+1-min));
+                for (int k = 0; k < i; k++) {
+                    if (arrayList[k] == (int) random) {
+                        flag = 1;
+                    }
+                }
+                if (flag == 0) {
+                    arrayList[i] = (int) random;
+                    System.out.println("so thu " + i + " " + arrayList[i]);
+                    break;
+                }*/
+
+            }
+        }
